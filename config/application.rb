65413e9ca7ab9267d07308b691d89fafbe712a7d
asharijuang <asharijuang@me.com>
2021-02-19 09:49:16 +0700

    another cors

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module Marketplace
     config.autoload_paths += %W[
       #{config.root}/app/services
     ]
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
   end
 end
